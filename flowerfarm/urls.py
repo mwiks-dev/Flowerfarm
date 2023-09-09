e9d3b9e05f8080d3ddaadb697b7054adb3453704
Maryann Mwikali <mwikali119@gmail.com>
2023-09-10 00:12:22 +0300

    Correct URL's and create login skeleton

--- flowerfarm/urls.py
+++ flowerfarm/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('greenhouse.urls'))
+    path('greenhouse/', include('greenhouse.urls')),
+    path('', RedirectView.as_view(url='greenhouse/')),
+    
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-]
