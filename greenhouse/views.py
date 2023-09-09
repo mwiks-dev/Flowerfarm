e2103d5e6ed59514f86aa03646f78db7d5a2e2fc
Maryann Mwikali <mwikali119@gmail.com>
2023-09-10 00:39:04 +0300

    Change model schema and staff number to models

--- greenhouse/views.py
+++ greenhouse/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
+from .models import User
+
 # Create your views here.
 
 #Home page
@@ -9,5 +11,6 @@ def index(request):
     return HttpResponse('Welcome Home')
 
 #Login Page
-def login(request):
+def login(request, id):
+    user = User.objects.filter(id = id)
     return redirect('/')
