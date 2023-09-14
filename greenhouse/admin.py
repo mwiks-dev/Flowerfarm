66878c87c6c33b1f9746d9f6f0206fe69e26dbe6
Maryann Mwikali <mwikali119@gmail.com>
2023-09-14 20:04:43 +0300

    Remove custome user

--- greenhouse/admin.py
+++ greenhouse/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Production, User
+from .models import Production
 
 # Register your models here.
 admin.site.register(Production)
-admin.site.register(User)
 
--- greenhouse/models.py
+++ greenhouse/models.py
@@ -1,39 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext as _
+from django.contrib.auth.models import User
+
 
 
 # Create your models here.
-
-# User class
-class User(AbstractUser):
-    # Define the extra fields related to User here
-    first_name = models.CharField(_('First Name of User'),null = True, max_length = 20)
-    last_name = models.CharField(_('Last Name of User'),null = True, max_length = 20)
-    email = models.EmailField(unique=True)
-    staff_number = models.IntegerField(null=True)
-
-    groups = models.ManyToManyField('auth.Group', verbose_name=_('groups'), blank=True, related_name='custom_user_groups')
-    user_permissions = models.ManyToManyField('auth.Permission', verbose_name=_('user permissions'), blank=True, related_name='custom_user_permissions')
-                              
-    #custom permissions related to User.
-    class Meta:
-        ordering = ['-staff_number']
-
-        permissions = (
-            ("can edit", "To provide edit form"),
-            ("can download reports", "To provide download access")
-            )
-    
-    def __str__(self):
-        return f"{self.username}"
-
 class Production(models.Model):
     production_date = models.DateField(auto_now_add=True)
     greenhouse_number = models.IntegerField()
     variety = models.CharField(max_length=50)
     length = models.DecimalField(max_digits=5, decimal_places=2)
-    staff_member = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    staff_member = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     rejected_flowers = models.IntegerField(null=True, blank=True)
     rejection_reason = models.CharField(max_length=100, blank=True, null=True)
 
