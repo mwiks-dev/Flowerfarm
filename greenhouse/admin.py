bd8c337bac965b009b4982ba353c41f8302aa7db
Maryann Mwikali <mwikali119@gmail.com>
2023-09-17 20:54:27 +0300

    Register profile

--- greenhouse/admin.py
+++ greenhouse/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Production
+from .models import Production, Profile
 
 # Register your models here.
 admin.site.register(Production)
+admin.site.register(Profile)
 
