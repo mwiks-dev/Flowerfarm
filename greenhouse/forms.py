545d93e6d03acd6ae83b9ea74ba7c53661c092c6
Mwikali <mwikali119@gmail.com>
2023-11-16 03:22:12 +0300

    Add form for rejected data

--- greenhouse/forms.py
+++ greenhouse/forms.py
@@ -1,9 +1,14 @@
 from django import forms
-from .models import Production
+from .models import Production, RejectedData
 
 class ProductionForm(forms.ModelForm):
     class Meta:
         model = Production
         fields = ['greenhouse_number','varieties','length']
+
+class RejectedDataForm(forms.ModelForm):
+    class Meta:
+        model = RejectedData
+        fields = ['greenhouse_number','varieties','rejected_number','length','rejection_reason']
 
     
